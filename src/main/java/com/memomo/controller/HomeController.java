63345f294f0364aa0dd1d2104aae19afe41f0bb2
chs1825 <jhsnotnull@gmail.com>
2023-03-27 23:25:18 +0900

    2023.03.23

--- src/main/java/com/memomo/controller/HomeController.java
+++ src/main/java/com/memomo/controller/HomeController.java
@@ -1,29 +1,30 @@
 package com.memomo.controller;
 
 import com.memomo.entity.User;
+import com.memomo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/main")
 public class HomeController {
 
-
-//    @Autowired
-//    private UserService userService;
+    @Autowired
+    private UserService service;
 
 
     @GetMapping("/")
     public String index(Model model) {
 
+        List<User> user = service.getUser();
+        model.addAttribute("data", user);
 
-//        Optional<User> user = userService.readList("chs1825");
-//        model.addAttribute("data", user);
 
         return "mainPage";
 
--- src/main/java/com/memomo/service/UserService.java
+++ src/main/java/com/memomo/service/UserService.java
@@ -6,6 +6,8 @@ import com.memomo.repository.UserRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -13,7 +15,33 @@ public class UserService {
     private UserRepository uR;
 
     public User getUserByName(String name){
+
         return uR.findByName(name);
+
     }
 
+    public User getUserById(String id){
+        return uR.findById(id);
+    }
+
+    public List<User> getUser(){
+        return uR.findAll();
+    }
+
+    public void saveUser(User user){
+        uR.save(user);
+    }
+
+
+    public void deleteUser(String id){
+        uR.deleteById(id);
+    }
+
+    public void updateUser(User user){
+        uR.update(user);
+    }
+
+
+
 }
+
